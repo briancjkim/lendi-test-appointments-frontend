1e6cc0ea71565c8478169ae275f0f24794d1274b
brian <briancjkim91@gmail.com>
2022-06-07 22:47:54 +1000

    update each component to render content based on data, and write test scenarios

--- app/src/components/Root/AppointmentSelect/Broker/Broker.test.tsx
+++ app/src/components/Root/AppointmentSelect/Broker/Broker.test.tsx
@@ -1,23 +1,89 @@
-import { screen, render } from "@testing-library/react";
+import {
+  screen,
+  render,
+  getByText,
+  fireEvent,
+  queryByText,
+} from '@testing-library/react'
+import Navigation from '../../Navigation'
+import AppointmentSelect from '../AppointmentSelect'
 
-import Broker from "./Broker";
+import Broker from './Broker'
 
 const testBroker = {
-  name: "bob",
+  name: 'bob',
   id: 1,
-  appointments: [{ brokerId: 1, date: "24/11/2021", id: 1 }],
-};
+  appointments: [{ brokerId: 1, date: '24/11/2021', id: 1 }],
+}
 
-describe("Broker Component", () => {
-  test("should hide and show appointments on button click", () => {
-    render(<Broker broker={testBroker} />);
+const selectedDetail = {
+  broker: { id: testBroker.id, name: testBroker.name },
+  appointment: testBroker.appointments[0],
+}
 
-    const showAppointmentsButton = screen.getByTestId(
-      "broker-show-appointments-button"
-    );
-    const hideAppointmentsButton = screen.getByTestId(
-      "broker-hide-appointments-button"
-    );
-    const appointmentsList = screen.getByTestId("broker-appointments-list");
-  });
-});
+//npx jest Broker
+describe('Broker Component', () => {
+  test('should hide and show appointments on button click', () => {
+    const { getByTestId, queryByTestId } = render(
+      <Broker broker={testBroker} setSelectedDetail={() => {}} />
+    )
+
+    const appointmentsList = getByTestId('broker-appointments-list')
+    getByText(appointmentsList, '24/11/2021')
+
+    const hideAppointmentsButton = getByTestId(
+      'broker-hide-appointments-button'
+    )
+    fireEvent.click(hideAppointmentsButton)
+    // not.toBeInTheDocument doesn't work => needs jest-dom package
+    expect(queryByTestId('broker-appointments-list')).toBeNull()
+
+    const showAppointmentsButton = getByTestId(
+      'broker-show-appointments-button'
+    )
+    fireEvent.click(showAppointmentsButton)
+
+    const reAppearedAppointmentsList = getByTestId('broker-appointments-list')
+    queryByText(reAppearedAppointmentsList, '24/11/2021')
+  })
+
+  test('should trigger clickHandler on appointment date', () => {
+    const setSelectedDetail = jest.fn()
+    const { getByTestId } = render(
+      <Broker broker={testBroker} setSelectedDetail={setSelectedDetail} />
+    )
+    const selectedAppointmentDate = getByTestId(
+      `broker-appointments-list-item-${testBroker.appointments[0].id}`
+    )
+    fireEvent.click(selectedAppointmentDate)
+    expect(setSelectedDetail).toBeCalled()
+  })
+})
+
+describe('AppointmentSelect component', () => {
+  test('should render appointment details text based on given prop', () => {
+    const { getByTestId, getByText } = render(
+      <AppointmentSelect
+        selectedDetail={selectedDetail}
+        setSelectedDetail={() => {}}
+      />
+    )
+    getByTestId('appointment-detail-section')
+    // The string is broken up by mutiple lines. so use exact:false text matcher option to match substring only
+    // more control over fuzzy maching than exact:false. https://stackoverflow.com/questions/65796123/getbytext-doesnt-find-the-element
+    // getByText('Appointment with Bob on 24/11/2021', {exact: false})
+    getByText(new RegExp('Appointment with bob on 24/11/2021', 'i'))
+  })
+})
+
+describe('Navigation component', () => {
+  test('should render appointment details text based on given prop', () => {
+    const { getByTestId, getByText } = render(
+      <Navigation selectedDetail={selectedDetail} />
+    )
+    getByTestId('navigation-appointment-detail')
+    getByText(
+      new RegExp('Currently selected appointment: 24/11/2021 with Bob', 'i')
+    )
+  })
+})
--- app/src/components/Root/AppointmentSelect/Broker/Broker.tsx
+++ app/src/components/Root/AppointmentSelect/Broker/Broker.tsx
@@ -1,25 +1,90 @@
-import React from "react";
+import React, { useCallback, useState } from 'react'
+import styled from 'styled-components'
+import { AppointmentDetail } from '../../Root'
+
+const StyledListItem = styled.li`
+  background-color: #f9e9e6;
+  &:not(:last-child) {
+    margin-bottom: 20px;
+  }
+`
+const StyledButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-itmes: center;
+`
+const StyledContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+`
 
 export interface BrokerProps {
   broker: {
-    name: string;
-    id: number;
-    appointments: { id: number; brokerId: number; date: string }[];
-  };
+    name: string
+    id: number
+    appointments: { id: number; brokerId: number; date: string }[]
+  }
+  setSelectedDetail: React.Dispatch<
+    React.SetStateAction<AppointmentDetail | null>
+  >
 }
 
-const Broker = (broker: BrokerProps) => {
-  return (
-    <li>
-      [broker name]
-      <br />
-      appointments:
-      <button>Hide appointments</button>
-      <ul>
-        <li>[appointment date]</li>
-      </ul>
-    </li>
-  );
-};
+const Broker = ({ broker, setSelectedDetail }: BrokerProps) => {
+  const [showAppointments, setShowAppointments] = useState(true)
 
-export default Broker;
+  const handleAppointmentDateClick = useCallback(
+    (appointment) => () => setSelectedDetail({ broker, appointment }),
+    [setSelectedDetail, broker]
+  )
+
+  const toggleShowAppointment = useCallback(
+    () => setShowAppointments(!showAppointments),
+    [showAppointments, setShowAppointments]
+  )
+
+  return (
+    <StyledListItem>
+      <StyledContainer>
+        {broker.name}
+        <StyledButtonWrapper>
+          <button
+            data-testid={
+              showAppointments
+                ? 'broker-hide-appointments-button'
+                : 'broker-show-appointments-button'
+            }
+            onClick={toggleShowAppointment}
+          >
+            {showAppointments ? 'Hide' : 'Show'}
+          </button>
+        </StyledButtonWrapper>
+      </StyledContainer>
+      {/* why use  ternary operator instead of &&*/}
+      {showAppointments ? (
+        <div
+          style={{ display: 'flex', justifyContent: 'space-between' }}
+          data-testid="broker-appointments-list"
+        >
+          appointments:
+          <ul style={{ cursor: 'pointer' }}>
+            {broker.appointments.length
+              ? broker.appointments.map((appointment) => (
+                  <li
+                    key={appointment.id}
+                    data-testid={`broker-appointments-list-item-${appointment.id}`}
+                    onClick={handleAppointmentDateClick(appointment)}
+                  >
+                    {appointment.date}
+                  </li>
+                ))
+              : null}
+          </ul>
+        </div>
+      ) : null}
+    </StyledListItem>
+  )
+}
+
+export default Broker
